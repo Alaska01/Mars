69939caadf531c30f77ea6c021de4e58df59fe9e
Aye Daniel A <aadaniel108@gmail.com>
2021-01-25 19:11:12 +0100

    Add pretty testing using Minitest

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
@@ -10,4 +11,7 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  require "minitest/reporters"
+  Minitest::Reporters.use!
+  
 end
